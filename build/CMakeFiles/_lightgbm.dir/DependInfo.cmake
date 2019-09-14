c2f7df24a0652379db0f04bed141387c715da064
appleyuchi <appleyuchi@163.com>
2019-09-14 11:43:06 +0800

    更新一些文件

---
+++ build/CMakeFiles/_lightgbm.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/content/sample_data/LightGBM/src/application/application.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/application/application.cpp.o"
+  "/content/sample_data/LightGBM/src/boosting/boosting.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/boosting/boosting.cpp.o"
+  "/content/sample_data/LightGBM/src/boosting/gbdt.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/boosting/gbdt.cpp.o"
+  "/content/sample_data/LightGBM/src/boosting/gbdt_model_text.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/boosting/gbdt_model_text.cpp.o"
+  "/content/sample_data/LightGBM/src/boosting/gbdt_prediction.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/boosting/gbdt_prediction.cpp.o"
+  "/content/sample_data/LightGBM/src/boosting/prediction_early_stop.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/boosting/prediction_early_stop.cpp.o"
+  "/content/sample_data/LightGBM/src/c_api.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/c_api.cpp.o"
+  "/content/sample_data/LightGBM/src/io/bin.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/bin.cpp.o"
+  "/content/sample_data/LightGBM/src/io/config.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/config.cpp.o"
+  "/content/sample_data/LightGBM/src/io/config_auto.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/config_auto.cpp.o"
+  "/content/sample_data/LightGBM/src/io/dataset.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/dataset.cpp.o"
+  "/content/sample_data/LightGBM/src/io/dataset_loader.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/dataset_loader.cpp.o"
+  "/content/sample_data/LightGBM/src/io/file_io.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/file_io.cpp.o"
+  "/content/sample_data/LightGBM/src/io/json11.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/json11.cpp.o"
+  "/content/sample_data/LightGBM/src/io/metadata.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/metadata.cpp.o"
+  "/content/sample_data/LightGBM/src/io/parser.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/parser.cpp.o"
+  "/content/sample_data/LightGBM/src/io/tree.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/io/tree.cpp.o"
+  "/content/sample_data/LightGBM/src/lightgbm_R.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/lightgbm_R.cpp.o"
+  "/content/sample_data/LightGBM/src/metric/dcg_calculator.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/metric/dcg_calculator.cpp.o"
+  "/content/sample_data/LightGBM/src/metric/metric.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/metric/metric.cpp.o"
+  "/content/sample_data/LightGBM/src/network/linker_topo.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/network/linker_topo.cpp.o"
+  "/content/sample_data/LightGBM/src/network/linkers_mpi.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/network/linkers_mpi.cpp.o"
+  "/content/sample_data/LightGBM/src/network/linkers_socket.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/network/linkers_socket.cpp.o"
+  "/content/sample_data/LightGBM/src/network/network.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/network/network.cpp.o"
+  "/content/sample_data/LightGBM/src/objective/objective_function.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/objective/objective_function.cpp.o"
+  "/content/sample_data/LightGBM/src/treelearner/data_parallel_tree_learner.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/treelearner/data_parallel_tree_learner.cpp.o"
+  "/content/sample_data/LightGBM/src/treelearner/feature_parallel_tree_learner.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/treelearner/feature_parallel_tree_learner.cpp.o"
+  "/content/sample_data/LightGBM/src/treelearner/gpu_tree_learner.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/treelearner/gpu_tree_learner.cpp.o"
+  "/content/sample_data/LightGBM/src/treelearner/serial_tree_learner.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/treelearner/serial_tree_learner.cpp.o"
+  "/content/sample_data/LightGBM/src/treelearner/tree_learner.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/treelearner/tree_learner.cpp.o"
+  "/content/sample_data/LightGBM/src/treelearner/voting_parallel_tree_learner.cpp" "/content/sample_data/LightGBM/build/CMakeFiles/_lightgbm.dir/src/treelearner/voting_parallel_tree_learner.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_GPU"
+  "USE_SOCKET"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../compute/include"
+  "/usr/local/cuda/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
